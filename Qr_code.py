235f769e03e8932fe5b090b35f407ccfd3547894
niteshparikh <niteshparikh08@gmail.com>
2022-09-22 10:48:31 +0530

    QR_Code

---
+++ Qr_code.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.append(r'C:\Users\niteshparikh\AppData\Local\Programs\Python\Python310\Lib\site-packages')
+import qrcode
+import image
+
+qr=qrcode.QRCode(
+    version=15,
+    box_size=10,
+    border=5
+)
+
+url_link = 'https://www.youtube.com'
+
+qr.add_data(url_link)
+qr.make(fit=True)
+img = qr.make_image(fill="black",back_color="white")
+img.save("scanner_img.png")
